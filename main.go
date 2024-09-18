46d50b95651cc92f567ee89a6f976d813227be73
Zakharchuk Denis <albertgutenberg1@gmail.com>
2024-09-18 18:19:17 +0300

    Created main structures

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"strconv"
+)
+
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+type Director struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+}
+
+var movies []Movie
+
+func main() {
+	r := mux.NewRouter()
+	//r.HandleFunc()
+}
